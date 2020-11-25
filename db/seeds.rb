94ad0f9edecb86f7402b6df66b377c3028e61438
tom chang <tomlin83@gmail.com>
2020-11-25 17:00:39 +0800

    updated until cards for index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Clearing existing database'
+
+Flat.destroy_all
+
+puts'creating flats'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Castle delight',
+  address: '10 somewhere liverpool DFFE#',
+  description: 'best place for honeymooners',
+  price_per_night: 1000,
+  number_of_guests: 2
+)
+
+
+Flat.create!(
+  name: 'Apartment ale',
+  address: '10 somewhere manchester adsfdfE#',
+  description: 'only if you like to watch football',
+  price_per_night: 500,
+  number_of_guests: 6
+)
+
+puts 'Completed creation'
